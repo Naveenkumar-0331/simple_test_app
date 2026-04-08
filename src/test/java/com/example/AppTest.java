7bbc351bab343f70818ffb70917d0c9500737db9
Naveenkumar-0331 <naveenkumarug03@gmail.com>
2026-04-08 13:10:18 +0530

    first commit

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+    public void testAddition() {
+        assertEquals(5, App.add(2, 3));
+    }
+}
+
